74aae3568ff43164f10d8144ec2a7aefe7a19bad
RajithaKWG <rkrishan894@gmail.com>
2025-02-27 23:19:52 +0530

    implemented signout

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -1,6 +1,8 @@
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import axios from 'axios';
 import {createContext, ReactNode, useState} from 'react';
 
-const API_URL = 'http://10.0.2.2:5000';
+const API_URL = 'http://192.168.43.221:5000';
 
 interface AuthContextData {
   token: string | null;
@@ -22,15 +24,67 @@ export const AuthProvider: React.FC<{children: ReactNode}> = ({children}) => {
 
   const signUp = async (email: string, password: string): Promise<boolean> => {
     console.log(email, password);
-    
-    return true;
+
+    try {
+      const result = await axios.post(`${API_URL}/api/auth/register`, {
+        email,
+        password,
+      });
+
+      if (result?.data?.success) return true;
+      else return false;
+    } catch (error) {
+      console.log(error);
+      if (axios.isAxiosError(error)) {
+        console.log(error.response?.data);
+      }
+
+      return false;
+    }
   };
 
   const signIn = async (email: string, password: string): Promise<boolean> => {
-    return true;
+    try {
+      const result = await axios.post(`${API_URL}/api/auth/login`, {
+        email,
+        password,
+      });
+
+      const {token, userId, success} = result?.data;
+
+      if (success) {
+        //Store token in async store
+        await AsyncStorage.setItem('token', token);
+        setToken(token);
+        await AsyncStorage.setItem('userId', userId);
+        setUserId(userId);
+
+        return true;
+
+      }else{
+        return false;
+      }
+    } catch (error) {
+      console.log(error);
+      if (axios.isAxiosError(error)) {
+        console.log(error.response?.data);
+      }
+
+      return false;
+    }
   };
 
-  const signOut = async (): Promise<void> => {};
+  const signOut = async (): Promise<void> => {
+    try{
+      await AsyncStorage.removeItem('token');
+      await AsyncStorage.removeItem('userId');
+      setUserId(null);
+      setToken(null)
+
+    }catch(e){
+      console.log(e);
+    }
+  };
 
   return (
     <AuthContext.Provider
@@ -41,6 +95,8 @@ export const AuthProvider: React.FC<{children: ReactNode}> = ({children}) => {
         signIn,
         signUp,
         signOut,
-      }}>{children}</AuthContext.Provider>
+      }}>
+      {children}
+    </AuthContext.Provider>
   );
 };
--- src/screens/LoginScreen.tsx
+++ src/screens/LoginScreen.tsx
@@ -1,6 +1,8 @@
 import { NativeStackNavigationProp } from "@react-navigation/native-stack";
-import { Button, Text, View } from "react-native";
+import { Alert, Button, StyleSheet, Text, TextInput, TouchableOpacity, View } from "react-native";
 import { RootStackParamsList } from "../navigation/RootNavigation";
+import { useContext, useState } from "react";
+import { AuthContext } from "../context/AuthContext";
 
 type LoginScreenNavigationProp = NativeStackNavigationProp<RootStackParamsList, 'Login'>;
 
@@ -10,14 +12,91 @@ interface LoginScreenProp {
 
 const LoginScreen: React.FC<LoginScreenProp> = ({navigation}) => {
 
+  const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const {signIn} = useContext(AuthContext);
+  
+    const handleSignIn = async () => {
+      if (email && password) {
+        const result = await signIn(email, password);
+        if (result) {
+          Alert.alert('Success', 'Login successfully!');
+          navigation.navigate('Home');
+        } else {
+          Alert.alert('Login failed!' );
+        }
+      } else {
+        Alert.alert('Invalid Input!');
+      }
+    };
+
   return (
-    <View>
-      <Text>
-        Login Screen
-      </Text>
-      <Button title="Go to Sign Up" onPress={() => navigation.navigate('SignUp')} />
-    </View>
+    <View style={styles.container}>
+          <Text style={styles.headrText}>Sign In</Text>
+          <TextInput
+            placeholder="Email"
+            keyboardType="email-address"
+            autoCapitalize="none"
+            style={styles.input}
+            value={email}
+            onChangeText={setEmail}
+          />
+          <TextInput
+            placeholder="Password"
+            secureTextEntry
+            style={styles.input}
+            value={password}
+            onChangeText={setPassword}
+          />
+          <TouchableOpacity onPress={handleSignIn} style={styles.botton}>
+            <Text style={styles.bottonText}>Sign In</Text>
+          </TouchableOpacity>
+          <TouchableOpacity onPress={() => navigation.navigate('SignUp')}>
+            <Text style={styles.linkText}>Don't have an account? Sign up</Text>
+          </TouchableOpacity>
+        </View>
   )
-}
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 20,
+  },
+  headrText: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 20,
+  },
+  input: {
+    width: '100%',
+    height: 45,
+    borderWidth: 1,
+    borderColor: '#cccccc',
+    borderRadius: 6,
+    paddingHorizontal: 10,
+    marginBottom: 10,
+  },
+  botton: {
+    width: '100%',
+    height: 40,
+    backgroundColor: '#007aff',
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 5,
+    marginTop: 10,
+  },
+  bottonText: {
+    color: '#ffffff',
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
+  linkText: {
+    marginTop: 16,
+    color: '#007aff',
+  },
+});
 
 export default LoginScreen;
